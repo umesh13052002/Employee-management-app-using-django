0bd7b3687c78733e14a3f30d18268bc56f42a09a
Umesh <25602umesh.2020@gmail.com>
2024-06-29 19:37:41 +0530

    Created a form and on submit data is posted on db

--- employee/views.py
+++ employee/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Emp
 # Create your views here.
 def student_home(request):
     return render(request, 'employee/index.html', {'message': 'Hello!'})
 def add_emp(request):
+    if request.method == "POST":
+        emp_name =request.POST.get("emp_name")
+        emp_id = request.POST.get("emp_id")
+        emp_phone = request.POST.get("emp_phone")
+        emp_address = request.POST.get("emp_address")
+        emp_working = request.POST.get("emp_working")
+        emp_department = request.POST.get("emp_department")
+
+        e=Emp()
+        e.emp_name = emp_name
+        e.emp_id = emp_id
+        e.phone = emp_phone
+        e.address = emp_address
+        e.department = emp_department
+        if emp_working is None:
+            e.working = False
+        else:
+            e.working = True
+            
+        e.save()
+        
+        return redirect("/student/home")
     return render(request, 'employee/add_emp.html',{})
